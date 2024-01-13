f9ab4053a5ea775a9bc564177a32ec295cce1d02
byhuang1998 <byhuang1998@gmail.com>
2024-01-13 19:58:47 +0800

    MyServlet

---
+++ src/main/java/com/byhuang/MyServlet.java
@@ -0,0 +1,59 @@
+package com.byhuang;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+public abstract class MyServlet implements Servlet {
+
+    @Override
+    public void init(ServletConfig servletConfig) throws ServletException {
+
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return null;
+    }
+
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+
+        String method = httpServletRequest.getMethod();
+
+        if ("GET".equals(method)) {
+            System.out.println("get method...");
+            // GET方法获取参数，并进行相应处理
+            doGet(servletRequest, servletResponse);
+        } else if ("POST".equals(method)) {
+            System.out.println("post method...");
+            // POST方法获取参数，并进行相应处理
+            doPost(servletRequest, servletResponse);
+        }
+        System.out.println("hello servlet!");
+    }
+
+
+    @Override
+    public String getServletInfo() {
+        return null;
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+
+    protected void doGet(ServletRequest servletRequest, ServletResponse servletResponse) {
+        // do something
+    }
+
+    protected void doPost(ServletRequest servletRequest, ServletResponse servletResponse) {
+        // do something
+    }
+
+        
+}
+
+    
--- src/main/java/com/byhuang/ServletDemo.java
+++ src/main/java/com/byhuang/ServletDemo.java
@@ -2,44 +2,17 @@ package com.byhuang;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServletRequest;
-import java.io.IOException;
 
 @WebServlet("/hello")
-public class ServletDemo implements Servlet {
+public class ServletDemo extends MyServlet {
 
     @Override
-    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
-        HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
-
-        String method = httpServletRequest.getMethod();
-
-        if ("GET".equals(method)) {
-            System.out.println("get method...");
-        } else if ("POST".equals(method)) {
-            System.out.println("post method...");
-        }
-        System.out.println("hello servlet!");
-
+    protected void doGet(ServletRequest servletRequest, ServletResponse servletResponse) {
+        super.doGet(servletRequest, servletResponse);
     }
 
     @Override
-    public void init(ServletConfig servletConfig) throws ServletException {
-
-    }
-
-    @Override
-    public ServletConfig getServletConfig() {
-        return null;
-    }
-
-    @Override
-    public String getServletInfo() {
-        return null;
-    }
-
-    @Override
-    public void destroy() {
-
+    protected void doPost(ServletRequest servletRequest, ServletResponse servletResponse) {
+        super.doPost(servletRequest, servletResponse);
     }
 }
