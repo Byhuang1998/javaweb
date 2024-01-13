d86db799660a21682fc9aa9fc210d0818b8ac5ba
byhuang1998 <byhuang1998@gmail.com>
2024-01-13 19:00:57 +0800

    servlet lifecycle

--- src/main/java/com/byhuang/ServletDemo.java
+++ src/main/java/com/byhuang/ServletDemo.java
@@ -1,4 +1,34 @@
 package com.byhuang;
 
-public class ServletDemo {
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
+
+@WebServlet("/hello")
+public class ServletDemo implements Servlet {
+
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+        System.out.println("hello servlet!");
+
+    }
+    @Override
+    public void init(ServletConfig servletConfig) throws ServletException {
+
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return null;
+    }
+
+    @Override
+    public String getServletInfo() {
+        return null;
+    }
+
+    @Override
+    public void destroy() {
+
+    }
 }
