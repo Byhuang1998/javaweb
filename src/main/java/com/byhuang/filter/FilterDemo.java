2bdd7940aa337745743955c5e3ee0448fe41a4f6
byhuang1998 <byhuang1998@gmail.com>
2024-01-21 00:44:43 +0800

    java web filter

---
+++ src/main/java/com/byhuang/filter/FilterDemo.java
@@ -0,0 +1,26 @@
+package com.byhuang.filter;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+
+@WebFilter("/*")
+public class FilterDemo implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        System.out.println("filter demo...");
+        // 一定要有这一行，表示处理完请求后，放行，会请求到对应的servlet
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
