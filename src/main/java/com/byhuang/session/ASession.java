59343d2d4aa115aaf6c1d98c3a3d247012451870
byhuang1998 <byhuang1998@gmail.com>
2024-01-20 19:47:02 +0800

    http session

---
+++ src/main/java/com/byhuang/session/ASession.java
@@ -0,0 +1,21 @@
+package com.byhuang.session;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet("/asession")
+public class ASession extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+        System.out.println(session);
+        System.out.println(session.getId());
+        session.setAttribute("user", "zs");
+    }
+}
